3c3c2d1b222c0d6893d16b0ad0794a19f04d37ca
Anisha Shrestha <shrestan314@gmail.com>
2019-03-14 23:29:58 -0500

    navigation

--- src/Components/PokemonList.js
+++ src/Components/PokemonList.js
@@ -5,59 +5,68 @@ import { withAlert } from "react-alert";
 import "../App.js";
 const url="https://intern-pokedex.myriadapps.com/api/v1/pokemon?page=";
 //let  clicks =17;
-var clicks;
 
-function handleClick (){
-    clicks = clicks+1;
-    //window.location.reload(true);
-};
 //fdsf
 export default class PokemonList extends React.Component {
-  state = {
-    pokemons: [],
 
-  };
-  //static
+    constructor(props) {
+        super(props);
+        this.state = {
+            pokemons: [],
+            clicks: 1
+        };
+        this.decrease = this.decrease.bind(this);
+        this.setState({clicks: this.state.clicks - 1});
+    }
 
-
-
-  componentDidMount() {
-      clicks =1;
-
-    axios
-      .get(
-          url+clicks
-      )
-      .then(res => {
-        console.log(res);
-        this.setState({ pokemons: res.data.data });
-      });
-  }
+    //static
 
 
-  render() {
-    return(
-//
-      <section>
-          <div>
-              <button onClick={handleClick()}> {clicks}&laquo; Previous</button>
-          </div>
-        {this.state.pokemons.map(pokemon => (
-          <div key={pokemon.id} className="cards">
-            <div className="title">{pokemon.name}</div>
-            <div className="image">
-              <img src={pokemon.image} />
+    decrease = () => {
+        console.log("decrease clicked")
+        this.setState({clicks: this.state.clicks - 1});
+        //this.setState(prevState =>{
+        //    return{clicks: prevState.clicks-1}
+        //});
+        axios
+            .get("https://intern-pokedex.myriadapps.com/api/v1/pokemon?page=" + this.state.clicks)
+            .then(res => {
+                console.log(res);
+                this.setState({pokemons: res.data.data});
+            });
+        console.log(this.state.clicks)
+        //alert(this.state.clicks)
+        // alert(this.clicks)
+    }
+
+    componentDidMount() {
+        axios
+            .get("https://intern-pokedex.myriadapps.com/api/v1/pokemon?page=" + this.state.clicks)
+            .then(res => {
+                console.log(res);
+                this.setState({pokemons: res.data.data});
+            });
+        console.log(this.state.clicks)
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.pokemons.map(pokemon => (
+                    <div key={pokemon.id} className="cards">
+                        <div className="title">{pokemon.name}
+                        </div>
+                        <div className="image">
+                            <img src={pokemon.image}/></div>
+                        <button className="btn1">{pokemon.types[1]}</button>
+                        <button className="btn2">{pokemon.types[0]}</button>
+                    </div>
+                ))}
+                <h1>
+                    <button onClick={() => this.decrease()}> Previous</button>
+                    // < button onClick={this.addition} className="next">Next &raquo;</button>
+                </h1>
             </div>
-
-              <button className="btn1">{pokemon.types[1]}</button>
-              <button className="btn2">{pokemon.types[0]}</button>
-
-          </div>
-        ))}
-
-      </section>
-
-
-    );
-  }
+        );
+    }
 }
