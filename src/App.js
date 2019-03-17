ca537985c6ce2af35c3c9b26d039c3d6c4d6a7ef
Anisha Shrestha <shrestan314@gmail.com>
2019-03-16 19:20:57 -0500

    navigation

--- src/App.js
+++ src/App.js
@@ -4,25 +4,10 @@ import "./App.css";
 import PokemonList from "./Components/PokemonList";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-push(){
-     // var a = PokemonList.setState(PokemonList.state.clicks =2)
-      document.getElementById(" poke");
-      //PokemonList.state.clicks =2;
-   // alert(a)
-
-
-
-}
-
   render() {
     return (
-
       <div className="App" id={"poke"}>
         <h1>Pokédex </h1>
-
 
         <PokemonList />
       </div>
--- src/Components/PokemonList.js
+++ src/Components/PokemonList.js
@@ -1,72 +1,90 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./materialize.css";
-import { withAlert } from "react-alert";
+import Filter from "./filter";
 import "../App.js";
-const url="https://intern-pokedex.myriadapps.com/api/v1/pokemon?page=";
-//let  clicks =17;
+const url = "https://intern-pokedex.myriadapps.com/api/v1/pokemon?page=";
 
-//fdsf
-export default class PokemonList extends React.Component {
+export default class PokemonList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemons: [],
+      clicks: 2,
+      showComponent: false
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            pokemons: [],
-            clicks: 1
-        };
-        this.decrease = this.decrease.bind(this);
-        this.setState({clicks: this.state.clicks - 1});
-    }
+  decrease = () => {
+    console.log("decrease clicked");
+    this.setState({ clicks: this.state.clicks - 1 });
+    axios
+      .get(
+        "https://intern-pokedex.myriadapps.com/api/v1/pokemon?page=" +
+          this.state.clicks
+      )
+      .then(res => {
+        console.log(res);
+        this.setState({ pokemons: res.data.data });
+      });
+    console.log(this.state.clicks);
+  };
 
-    //static
+  increase = () => {
+    console.log("decrease clicked");
+    this.setState({ clicks: this.state.clicks + 1 });
+    axios
+      .get(
+        "https://intern-pokedex.myriadapps.com/api/v1/pokemon?page=" +
+          this.state.clicks
+      )
+      .then(res => {
+        console.log(res);
+        this.setState({ pokemons: res.data.data });
+      });
+    console.log(this.state.clicks);
+  };
+  search = event => {
+    event.preventDefault();
+    this.setState({
+      showComponent: true
+    });
+  };
 
+  componentDidMount() {
+    axios
+      .get("https://intern-pokedex.myriadapps.com/api/v1/pokemon?")
+      .then(res => {
+        console.log(res);
+        this.setState({ pokemons: res.data.data });
+      });
+    console.log(this.state.clicks);
+  }
 
-    decrease = () => {
-        console.log("decrease clicked")
-        this.setState({clicks: this.state.clicks - 1});
-        //this.setState(prevState =>{
-        //    return{clicks: prevState.clicks-1}
-        //});
-        axios
-            .get("https://intern-pokedex.myriadapps.com/api/v1/pokemon?page=" + this.state.clicks)
-            .then(res => {
-                console.log(res);
-                this.setState({pokemons: res.data.data});
-            });
-        console.log(this.state.clicks)
-        //alert(this.state.clicks)
-        // alert(this.clicks)
-    }
+  render() {
+    return (
+      <div>
+        <p>
+          <button onClick={() => this.decrease()}> Previous</button>
+          <button onClick={() => this.increase()}> Next</button>
+          <form onSubmit={this.search}>
+            <input type="text" placeholder="Pokedex" />
+            <button>Submit</button>
+          </form>
+          {this.state.showComponent ? <Filter /> : null}
+        </p>
 
-    componentDidMount() {
-        axios
-            .get("https://intern-pokedex.myriadapps.com/api/v1/pokemon?page=" + this.state.clicks)
-            .then(res => {
-                console.log(res);
-                this.setState({pokemons: res.data.data});
-            });
-        console.log(this.state.clicks)
-    }
-
-    render() {
-        return (
-            <div>
-                {this.state.pokemons.map(pokemon => (
-                    <div key={pokemon.id} className="cards">
-                        <div className="title">{pokemon.name}
-                        </div>
-                        <div className="image">
-                            <img src={pokemon.image}/></div>
-                        <button className="btn1">{pokemon.types[1]}</button>
-                        <button className="btn2">{pokemon.types[0]}</button>
-                    </div>
-                ))}
-                <h1>
-                    <button onClick={() => this.decrease()}> Previous</button>
-                    // < button onClick={this.addition} className="next">Next &raquo;</button>
-                </h1>
+        {this.state.pokemons.map(pokemon => (
+          <div key={pokemon.id} className="cards">
+            <div className="title">{pokemon.name}</div>
+            <div className="image">
+              <img src={pokemon.image} />
             </div>
-        );
-    }
+            <button className="btn1">{pokemon.types[1]}</button>
+            <button className="btn2">{pokemon.types[0]}</button>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
